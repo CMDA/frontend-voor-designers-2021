f0c5275ab7caa72dcec9c2bc2a2911839c271fd6
koopReynders <mail@kopo.nl>
2021-03-11 12:19:56 +0100

    demo

--- opdracht3/XHRjson/script.js
+++ opdracht3/XHRjson/script.js
@@ -1,96 +1,141 @@
-var url = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/";
-var urlmovies = url+"movies.json";
+var json = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json";
+console.log("json")
 
+ //setup JSON lader met XMLHttpRequest
+ var request = new XMLHttpRequest();
+ request.open('get', json);
+ request.responseType = 'json';
+ request.send();
 
-
-//////////////////////////////////////////////
-var sectionmovies = document.querySelector('#movies');
-console.log("section",sectionmovies);
-
-//setup JSON lader met XMLHttpRequest
-var request = new XMLHttpRequest();
-console.log("loadimagesmetXHR", request);
-request.open('get', urlmovies);
-request.responseType = 'json';
-request.send();
-
-request.addEventListener("load", function(){
-  //er is data
-  console.log("request is geladen: ",request.response);
+ request.addEventListener("load", function(){
   //nu kun je iets doen
-  renderHTML(request.response);
+  console.log("hebbes")
+  console.log(request.response);
+  console.log(request.response[4].cover)
+
+  //loop door de json nodes:
+  //html elementen maken: article, h2, p, img
+  //json data koppelen
+
 });
 
-function renderHTML(data){
-  console.log("renderHTML")
-  //section.textContent = JSON.stringify(data);
-  //loop door alle nodes van de JSON
-  for (var item of data) {
-    var article = document.createElement('article');
-    //film titel
-    var title = document.createElement('h2');
-    title.textContent = item.title;
-    //film plot
-    var plot = document.createElement('p');
-    plot.textContent = item.simple_plot;
-    //film poster
-    var cover = document.createElement('img');
-    cover.src = item.cover;
-    //koppel de html elementen aan het <article>
-    article.appendChild(title);
-    article.appendChild(plot);
-    article.appendChild(cover);
-    //koppel de <article> aan de section op de pagina
-    sectionmovies.appendChild(article);
-  };
+
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+  console.log("logKey",e.key, e.keyCode)
 }
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 //////////////////////////////////////////////
-var buttonlaadfilm = document.querySelector('button[name="laadfilm"]');
-var sectionfilm = document.querySelector('#film');
-console.log("button",buttonlaadfilm);
-console.log("section",sectionfilm);
 
-buttonlaadfilm.onclick = function(){
-  event.preventDefault();
+function code(){
 
-  var welkefilm = document.querySelector('input[name = "toggle"]:checked').value;
-  console.log("buttonlaadfilm",welkefilm);
+  var urlmovies = url+"movies.json";
+
+
+  var sectionmovies = document.querySelector('#movies');
+  console.log("section",sectionmovies);
 
   //setup JSON lader met XMLHttpRequest
   var request = new XMLHttpRequest();
-  request.open('get', url+welkefilm+".json");
+  console.log("loadimagesmetXHR", request);
+  request.open('get', urlmovies);
   request.responseType = 'json';
   request.send();
 
   request.addEventListener("load", function(){
-    var filmdata = request.response;
-    //er is data:
-    console.log(filmdata)
-    //film titel
-    var title = document.createElement('h2');
-    title.textContent = filmdata.title;
-    //film plot
-    var plot = document.createElement('p');
-    plot.textContent = filmdata.simple_plot;
-    //film trailer
-    var video = document.createElement('video');
-    video.src = filmdata.trailer;
-    video.controls = "true";
-    video.width = "360";
-    video.poster = filmdata.cover;
-
-    //koppel de html elementen aan de <section>
-    //eerst de section leeghalen
-    while (sectionfilm.firstChild) {
-      sectionfilm.removeChild(sectionfilm.firstChild);
-    }
-    sectionfilm.appendChild(title);
-    sectionfilm.appendChild(plot);
-    sectionfilm.appendChild(video);
+    //er is data
+    console.log("request is geladen: ",request.response);
+    //nu kun je iets doen
+    renderHTML(request.response);
   });
-};
+
+  function renderHTML(data){
+    console.log("renderHTML")
+    //section.textContent = JSON.stringify(data);
+    //loop door alle nodes van de JSON
+    for (var item of data) {
+      var article = document.createElement('article');
+      //film titel
+      var title = document.createElement('h2');
+      title.textContent = item.title;
+      //film plot
+      var plot = document.createElement('p');
+      plot.textContent = item.simple_plot;
+      //film poster
+      var cover = document.createElement('img');
+      cover.src = item.cover;
+      //koppel de html elementen aan het <article>
+      article.appendChild(title);
+      article.appendChild(plot);
+      article.appendChild(cover);
+      //koppel de <article> aan de section op de pagina
+      sectionmovies.appendChild(article);
+    };
+  }
+
+  //////////////////////////////////////////////
+  var buttonlaadfilm = document.querySelector('button[name="laadfilm"]');
+  var sectionfilm = document.querySelector('#film');
+  console.log("button",buttonlaadfilm);
+  console.log("section",sectionfilm);
+
+  buttonlaadfilm.onclick = function(){
+    event.preventDefault();
+
+    var welkefilm = document.querySelector('input[name = "toggle"]:checked').value;
+    console.log("buttonlaadfilm",welkefilm);
+
+    //setup JSON lader met XMLHttpRequest
+    var request = new XMLHttpRequest();
+    request.open('get', url+welkefilm+".json");
+    request.responseType = 'json';
+    request.send();
+
+    request.addEventListener("load", function(){
+      var filmdata = request.response;
+      //er is data:
+      console.log(filmdata)
+      //film titel
+      var title = document.createElement('h2');
+      title.textContent = filmdata.title;
+      //film plot
+      var plot = document.createElement('p');
+      plot.textContent = filmdata.simple_plot;
+      //film trailer
+      var video = document.createElement('video');
+      video.src = filmdata.trailer;
+      video.controls = "true";
+      video.width = "360";
+      video.poster = filmdata.cover;
+
+      //koppel de html elementen aan de <section>
+      //eerst de section leeghalen
+      while (sectionfilm.firstChild) {
+        sectionfilm.removeChild(sectionfilm.firstChild);
+      }
+      sectionfilm.appendChild(title);
+      sectionfilm.appendChild(plot);
+      sectionfilm.appendChild(video);
+    });
+  };
+
+} //end: function code
